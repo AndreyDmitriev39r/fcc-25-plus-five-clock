4c0961194c8ccd60c1d0bb2543f8502c85428b58
Andrey Dmitriev <admit8490@gmail.com>
2023-03-21 15:59:31 +0200

    heading-styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import SessionControl from "./SessionControl"
 
 function App() {
   return (
-    <div className="App flex flex-row flex-wrap gap-6 justify-center">
+    <div className="App flex flex-row flex-wrap gap-6 justify-center border border-black">
       <Heading />
       <LengthSettings name="session"/>
       <LengthSettings name="break"/>
--- src/Heading.jsx
+++ src/Heading.jsx
@@ -1,7 +1,7 @@
 const Heading = () => {
   return (
     <div className="heading-container w-full">
-      <h1 className="text-center mt-4 mb-1">Twenty five plus five clock</h1>
+      <h1 className="w-fit mx-auto mt-4 mb-1 px-3 py-1 bg-lime-400 text-center text-2xl uppercase italic font-extrabold tracking-wider text-slate-800 rounded-md">25 + 5 clock</h1>
     </div>
   )
 }
