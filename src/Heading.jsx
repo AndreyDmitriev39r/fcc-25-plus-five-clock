7ce63b552362e4c5c5f578b68075a609d50a204d
Andrey Dmitriev <admit8490@gmail.com>
2023-03-21 16:44:27 +0200

    heading and lengthsettings styling

--- src/Heading.jsx
+++ src/Heading.jsx
@@ -1,7 +1,9 @@
 const Heading = () => {
   return (
     <div className="heading-container w-full">
-      <h1 className="w-fit mx-auto mt-4 mb-1 px-3 py-1 bg-lime-400 text-center text-2xl uppercase italic font-extrabold tracking-wider text-slate-800 rounded-md">25 + 5 clock</h1>
+      <h1 className="w-fit mx-auto mt-4 px-3 py-1 bg-lime-400 text-center text-2xl uppercase italic font-extrabold tracking-wider text-slate-800 rounded-md">
+        25 + 5 clock
+      </h1>
     </div>
   )
 }
--- src/LengthSettings.jsx
+++ src/LengthSettings.jsx
@@ -1,8 +1,19 @@
 const LengthSettings = ({name}) => {
   return (
-    <div className={`${name}-settings-container`}>
-      <h3 id={`${name}-label`} className="capitalize">{name} length</h3>
-      <h3 id={`${name}-length`}>{name === 'session' ? '25' : '5'}</h3>
+    <div
+      id={`${name}-settings-container`}
+      className='w-1/4 min-w-[175px] max-w-[250px] flex flex-col gap-1 p-2 bg-lime-300 rounded-lg'
+    >
+      <h3 id={`${name}-label`}
+        className={`${name === 'session' ? "bg-blue-200" : "bg-amber-200"} text-center text-xl font-medium italic capitalize rounded-md`}
+      >
+          {name} length
+      </h3>
+      <h3 id={`${name}-length`}
+        className={`${name === 'session' ? "bg-blue-200" : "bg-amber-200"} w-10 h-10 self-center flex justify-center items-center my-2 rounded-full border border-lime-500 text-xl font-bold italic`}
+      >
+        {name === 'session' ? '25' : '5'}
+      </h3>
       <button id={`${name}-increment`}>+</button>
       <button id={`${name}-decrement`}>-</button> 
     </div>
